a321362d2e73549cf4cc09584aab659f49e54699
Mohsen Shabanian <mohsen1299@gmail.com>
2019-05-27 10:57:24 +0430

    Converter for Persian Date

---
+++ PersianDateExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace villaayab.Data.Utilities
+{
+    public static class PersianDateExtension
+    {
+        public static string PersianDate(this DateTime dateTime)
+        {
+            var persianCalendar = new PersianCalendar();
+
+            return persianCalendar.GetYear(dateTime).ToString("0000") +
+                   persianCalendar.GetMonth(dateTime).ToString("00") +
+                   persianCalendar.GetDayOfMonth(dateTime).ToString("00");
+        }
+
+        public static string PersianDateTime(this DateTime dateTime)
+        {
+            var persianCalendar = new PersianCalendar();
+
+            return persianCalendar.GetYear(dateTime).ToString("0000") + "/" +
+                   persianCalendar.GetMonth(dateTime).ToString("00") + "/" +
+                   persianCalendar.GetDayOfMonth(dateTime).ToString("00") + " " +
+                   dateTime.ToString("HH:mm:ss");
+        }
+    }
+}
