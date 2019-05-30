99712f0f544a9d25ec7d09569d4f51baca095629
mohsen shabanian <mohsen1299@gmail.com>
2019-05-30 10:26:17 +0430

    add Money extention

---
+++ MoneyExtensions.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Data.Utilities
+{
+    public static class MoneyExtensions
+    {
+        public static string TomanValue(this decimal money, bool simple = true)
+        {
+            if (simple || money < 1000)
+                return money.ToString("N0") + " تومان";
+            if (money < 1000000)
+                return (money / 1000).ToString("N3") + " هزار تومان";
+            return (money / 1000000).ToString("N3") + " میلیون تومان";
+        }
+
+    }
+}
